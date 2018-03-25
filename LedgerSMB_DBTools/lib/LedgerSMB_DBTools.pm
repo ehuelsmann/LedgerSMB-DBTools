38692829dc8fd96cdeed784ad0f00450f8ec5a75
Erik Huelsmann <ehuels@gmail.com>
2018-03-26 00:13:04 +0200

    * Add database plugin/keyword using for LedgerSMB's db/auth use

--- LedgerSMB_DBTools/lib/LedgerSMB_DBTools.pm
+++ LedgerSMB_DBTools/lib/LedgerSMB_DBTools.pm
@@ -1,6 +1,7 @@
 package LedgerSMB_DBTools;
 
 use Dancer2;
+use Dancer2::Plugin::SessionDatabase;
 use Dancer2::Plugin::Auth::Extensible;
 
 our $VERSION = '0.1';
@@ -10,6 +11,7 @@ get '/' => require_login sub {
 };
 
 get '/remove-invoice' => require_login sub {
+    database->do('select count(*) from pg_roles');
     template 'remove-invoice' => { 'title' => 'LedgerSMB_DBTools' };
 };
 
